90effc16291fa934e3f2f90377ecaed76886acad
prakashdanish <grafitykoncept@gmail.com>
2017-03-04 14:15:53 +0530

    added exc 5

--- hackerrank/warmup_5.py
+++ hackerrank/warmup_5.py
@@ -3,13 +3,17 @@ n = int(input())
 lst = []
 for i in range(n):
     lst.append([int(x) for x in input().split()])
+
 ldiag = 0
+rdiag = 0
 
 for i in range(len(lst)):
     for j in range(len(lst[0])):
         if i == j:
             ldiag += lst[i][j]
+        if (i+j) == n-1:
+            rdiag += lst[i][j]
+abSum = abs(ldiag-rdiag)
 
-
-print(ldiag)
+print(abSum)
 #print(lst)
