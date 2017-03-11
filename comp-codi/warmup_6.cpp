c849c30fe9d46ec12e8cb3bd35fe7d22a405b8b7
prakashdanish <grafitykoncept@gmail.com>
2017-03-11 11:55:53 +0530

    .

--- comp-codi/warmup_6.cpp
+++ comp-codi/warmup_6.cpp
@@ -8,13 +8,9 @@ int main()
   for(int i=1; i<=n; i++)
   {
     for(int j=n; j>i; j--)
-    {
       cout<<" ";
-    }
     for(int k=1; k<=i; k++)
-    {
       cout<<"#";
-    }
     cout<<endl;
   }
 }
