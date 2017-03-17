32bf15609ad76c4437acd03eaedc0115c8107a6d
prakashdanish <grafitykoncept@gmail.com>
2017-03-17 17:13:40 +0530

    .

---
+++ comp-codi/implementation_3.py
@@ -0,0 +1,16 @@
+x1, v1, x2, v2 = [int(s) for s in input().split()]
+gap = abs(x1-x2)
+while(True):
+    # if (x1 <= x2 and v1 < v2):
+    #     print("NO")
+    #     break
+    x1 += v1
+    x2 += v2
+    pregap = gap
+    gap = abs(x1 - x2)
+    if gap == 0:
+        print("YES")
+        break
+    elif gap >= pregap or (x1 <= x2 and v1 < v2):
+            print("NO")
+            break
