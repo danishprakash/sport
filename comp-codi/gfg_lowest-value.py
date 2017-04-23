2c157a635da271f29d33c32332be189d675a52f4
Danish Prakash <grafitykoncept@gmail.com>
2017-04-23 14:05:37 +0530

    updated from desktop-ubuntu

--- comp-codi/gfg_lowest-value.py
+++ comp-codi/gfg_lowest-value.py
@@ -1,23 +1,22 @@
 t = int(input())
-s = input()
-k = int(input())
-
 
 for i in range(t):
-    min_ = 2500
+    s = input()
+    k = int(input())
+    if k >= len(s)-1:
+        print('0')
+        continue
+    fval = 5000
     for i in range(len(s)-k+1):
         a = s[0:i]+s[i+k:]
+        minVal = 0
         temp = []
         #print(a)
-        minVal = 0
         for j in range(len(a)):
             if a[j] not in temp:
                 minVal += (a.count(a[j])**2)
                 temp.append(a[j])
-            print(minVal)
-        if minVal < min_:
-            min_ = minVal
-    if min_ == 2500:
-        print('0')
-    else:
-        print(min_)
+            #print(minVal)
+        if minVal < fval:
+            fval = minVal
+    print(fval)
