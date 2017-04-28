396bdefe8659ffef61bfc9e0752d5355876bed38
prakashdanish <grafitykoncept@gmail.com>
2017-04-28 20:48:07 +0530

    UF

---
+++ comp-codi/gfg_encode.py
@@ -0,0 +1,12 @@
+def encode(arr):
+    enc = ''
+    for i in range(len(str(arr))):
+        currcount = 0
+        while
+    return enc
+
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        arr=input().strip()
+        print (encode(arr))
