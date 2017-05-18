ad33d4e14cd07ac36f9a022ccbb6c171defc6a74
prakashdanish <grafitykoncept@gmail.com>
2017-05-18 10:28:04 +0530

    optimized

--- comp-codi/geeksforgeeks/magica_string.py
+++ comp-codi/geeksforgeeks/magica_string.py
@@ -4,15 +4,15 @@ for i in range(t):
     n = input()
     dic = dict()
     dic2 = dict()
-    inc = 25
+    inc, inc2 = [25, 1]
     s = ''
-    for i in range(97, 110):
-        dic[i] = i+inc
-        inc -= 2
-    inc2 = 1
-    for j in range(110, 123):
-        dic2[j] = j-inc2
-        inc2 += 2
+    for i in range(97, 123):
+        if i < 110:
+            dic[i] = i+inc
+            inc -= 2
+        elif i > 109:
+            dic2[i] = i-inc2
+            inc2 += 2
     for item in n:
         if ord(item) in dic.keys():
             s += chr(dic[ord(item)])
