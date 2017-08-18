453a5c8f65ea786b7f10e9de78a11c97568515d0
prakashdanish <grafitykoncept@gmail.com>
2017-08-18 20:52:49 +0530

    hr: electronics_shop

---
+++ comp-codi/hackerrank/algorithms/implementation/electronics_shop.py
@@ -0,0 +1,14 @@
+s, k, u = [int(x) for x in input().split()]
+key = [int(x) for x in input().split()]
+usb = [int(x) for x in input().split()]
+max_ = -1
+
+for i in range(k):
+    for j in range(u):
+        if key[i]+usb[j] > s:
+            continue
+        if key[i]+usb[j] > max_:
+            max_ = key[i]+usb[j]
+print(max_)
+
+
