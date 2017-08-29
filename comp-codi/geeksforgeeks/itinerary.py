3c50e994bf3f6e281abaf9d05dfc20132359ecba
prakashdanish <grafitykoncept@gmail.com>
2017-08-29 21:25:05 +0530

    gfg: itinerary.py

---
+++ comp-codi/geeksforgeeks/itinerary.py
@@ -0,0 +1,21 @@
+''' Input:
+"Chennai" -> "Banglore"
+"Bombay" -> "Delhi"
+"Goa"    -> "Chennai"
+"Delhi"  -> "Goa"
+
+Output: 
+Bombay->Delhi, Delhi->Goa, Goa->Chennai, Chennai->Banglore, '''
+
+d = dict()
+n = int(input())
+for i in range(n):
+    a, b = input().split()
+    d[a] = b
+for key in d.keys():
+    if key not in d.values():
+        source = key
+        break
+for i in range(len(d)):
+    print(source + ' -> ' + d[source] + ', ', end='')
+    source = d[source]
