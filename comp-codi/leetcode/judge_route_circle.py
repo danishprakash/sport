fc933259e479f89ce5221d84dfb0e56b4d847ff4
prakashdanish <grafitykoncept@gmail.com>
2017-09-06 14:52:44 +0530

    lc: judge_route_circle

---
+++ comp-codi/leetcode/judge_route_circle.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/judge-route-circle
+
+s = input()
+x, y =[0, 0]
+
+for i in s:
+    if i == 'U':
+        y += 1
+    elif i == 'D':
+        y -= 1
+    elif i == 'R':
+        x += 1
+    elif i == 'L':
+        x -= 1
+if x == 0 and y == 0:
+    print('true')
+else:
+    print('false')
