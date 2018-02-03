4c6fdce0d6b31effc7b9b60e14ad92429064f435
prakashdanish <grafitykoncept@gmail.com>
2018-02-03 21:45:17 +0530

    lc: reverse integer

--- comp-codi/leetcode/reverse_integer.py
+++ comp-codi/leetcode/reverse_integer.py
@@ -1,10 +1,25 @@
-class Solution:
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        a = ''
-        if str(x)[0] == '-':
-            a = '-'
-        return int(a + ''.join(list(map(str, ([int(x) for x in str(x) if ord(x) > 45][::-1])))))
+def reverse(x):
+    """
+    :type x: int
+    :rtype: int
+    """
+
+    if abs(x) > (2**31)-1:
+        return 0
+
+    x = list(str(x))
+
+    if x[0] == '-':
+        flag = True
+    else:
+        flag = False
+
+    if flag is True:
+        del x[0]
+        res = -(int(''.join(x[::-1])))
+        return res if abs(res) < (2**31)-1 else 0
+    else:
+        res = int(''.join(x[::-1]))
+        return res if abs(res) < (2**31)-1 else 0
+
+print(reverse(int(input())))
